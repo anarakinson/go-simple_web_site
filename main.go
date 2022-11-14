4223131ea595d3fc9355fcd03f9952b03371864b
anarakinson <anarakinson@gmail.com>
2022-11-14 21:33:47 +0300

    add contacts page

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 )
 
 func main() {
+    handleRequest()
+}
+
+func handleRequest() {
     http.HandleFunc("/", home_page)
+    http.HandleFunc("/contacts/", contacts_page)
     http.ListenAndServe(":8084", nil)
 }
 
-func home_page(page http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(page, "Hello")
+func home_page(w http.ResponseWriter, request *http.Request) {
+    fmt.Fprintf(w, "Hello")
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Contacts")
 }
