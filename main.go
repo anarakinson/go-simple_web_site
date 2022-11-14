8a73fba75629b119d379e1b8e8666eb9994539c9
anarakinson <anarakinson@gmail.com>
2022-11-14 23:12:05 +0300

    add html

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
 )
 
-func main() {
-    handleRequest()
+type User struct {
+    Name string
+    Age uint16
+    Money int
+    Avg_grades float64
+    Happiness float64
+    Hobbies []string
 }
+
+func (u User) getInfo() (string) {
+    return fmt.Sprintf(
+        "User name is %s. He is %d y.o., and he have %d money.",
+        u.Name, u.Age, u.Money,
+    )
+}
+
+func (u *User) setName(new_name string) {
+    u.Name = new_name
+}
+
 
 func handleRequest() {
     http.HandleFunc("/", home_page)
@@ -16,9 +34,28 @@ func handleRequest() {
 }
 
 func home_page(w http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(w, "Hello")
+    bobby := User{
+        Name: "Bobby",
+        Age: 25,
+        Money: -50,
+        Avg_grades: 4.5,
+        Happiness: 0.8,
+        Hobbies: []string{"Sex", "Drugs", "Rock-n-Roll"},
+    }
+
+    templ, err := template.ParseFiles("templates/homepage.html")
+    if (err == nil) {
+        templ.Execute(w, bobby)
+    } else {
+        fmt.Println(err)
+    }
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Contacts")
+}
+
+
+func main() {
+    handleRequest()
 }
